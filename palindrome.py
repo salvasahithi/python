ca82543c37286d07f8b18ba260796072d6daf873
salvasahithi <112721196+salvasahithi@users.noreply.github.com>
2022-09-12 09:46:47 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+num=int(input("enter a number"))
+x=num
+rev=0
+while num>0:
+    r=num%10
+    rev=(rev*10)+r
+    num=num//10
+print("reverse of given number is ",rev)
+if x==rev:
+    print("the num is a palindrome")
+else:
+    print("the num is not a palindrome")
+    
